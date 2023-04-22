3d177bd8b7ca1298b877a9ad033772f39c9cc461
Showrab15 <supto50showrab@gmail.com>
2023-04-22 11:39:59 +0600

    first-commit

---
+++ index.js
@@ -0,0 +1,22 @@
+
+//task-1
+const groceries =["banana", "apple", "peanuts"]
+if(groceries.indexOf("banana")){
+    console.log("We have To buy bananas")
+}
+else{
+    console.log("We don't have to buy bananas")
+}
+
+//output : We don't have to buy bananas
+
+//task-2
+const length = 4;
+const numbers =[];
+
+for(var i =0; i < length; i++){
+    numbers.push( i + 1)
+}
+console.log(numbers);
+
+//output : [ 1, 2, 3, 4 ]
